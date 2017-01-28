bedb4f75111e075ca9fc0c14cab80c512d636a5a
Zack Worden <zackworden@gmail.com>
2017-01-28 18:11:57 -0500

    Initial project setup.

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+
+// all api keys and other private info / security hazards belongs here, outside public repo
+require_once '/../../keys/keys.php';
+
+// all theme-specific settings go here
+require_once 'includes/themeSettings.php';
+
+// custom post type definitions
+require_once 'includes/postTypes.php';
+
+// custom taxonomy definitions
+require_once 'includes/taxonomy.php';
+
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,10 @@
+// README //
+
+
+PURPOSE
+
+
+METHODS
+
+
+
